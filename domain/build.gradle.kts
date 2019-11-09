531676411ee4ad1d53cf24053d960595b968bfca
Rivu <rivu.chakraborty6174@gmail.com>
2019-11-09 21:44:10 +0530

    chore: create domain module

---
+++ domain/build.gradle.kts
@@ -0,0 +1,30 @@
+import org.gradle.kotlin.dsl.*
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Config.targetSdkVersion)
+
+
+    defaultConfig {
+        minSdkVersion(Config.minSdkVersion)
+        targetSdkVersion(Config.targetSdkVersion)
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    }
+
+}
+
+dependencies {
+    implementation(Dependencies.kotlin)
+
+    testImplementation(Dependencies.Test.junit)
+}
