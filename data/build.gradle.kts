4aec18bd0c71c8f12340fa3782966982997b37da
Rivu <rivu.chakraborty6174@gmail.com>
2019-11-10 11:22:24 +0530

    chore: create data module

---
+++ data/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.gradle.kotlin.dsl.*
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Config.targetSdkVersion)
+
+
+    defaultConfig {
+        minSdkVersion(Config.minSdkVersion)
+        targetSdkVersion(Config.targetSdkVersion)
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFile("consumer-rules.pro")
+    }
+
+}
+
+dependencies {
+    implementation(project(":domain"))
+
+    implementation(Dependencies.kotlin)
+
+    //Rx
+    implementation(Dependencies.Rx.rxJava2)
+    implementation(Dependencies.Rx.rxAndroid2)
+
+    //Dagger
+    implementation(Dependencies.Dagger.core)
+    kapt(Dependencies.Dagger.compiler)
+
+    testImplementation(Dependencies.Test.mockito)
+    testImplementation(Dependencies.Test.mockitoKotlin)
+    testImplementation(Dependencies.Test.hamcrest)
+    testImplementation(Dependencies.Test.junit)
+}
