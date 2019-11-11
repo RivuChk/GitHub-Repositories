26f33a5fa54bc0a93cd6d5ecb94fa3aff94601ce
Rivu <rivu.chakraborty6174@gmail.com>
2019-11-11 23:57:49 +0530

    feat: implement popup window as per design

--- app/src/main/java/dev/rivu/githubrepositories/trendingprojects/TrendingProjectsActivity.kt
+++ app/src/main/java/dev/rivu/githubrepositories/trendingprojects/TrendingProjectsActivity.kt
@@ -1,9 +1,6 @@
 package dev.rivu.githubrepositories.trendingprojects
 
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import androidx.core.content.ContextCompat
+import android.view.*
 import androidx.lifecycle.ViewModelProviders
 import dev.rivu.githubrepositories.R
 import dev.rivu.githubrepositories.base.BaseMviActivity
@@ -21,6 +18,13 @@ import kotlinx.android.synthetic.main.content_trending_projects.*
 import kotlinx.android.synthetic.main.activity_trending_projects.*
 import timber.log.Timber
 import javax.inject.Inject
+import android.widget.PopupWindow
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.content.Context
+import android.os.Build
+import kotlinx.android.synthetic.main.layout_menu.view.*
+
 
 class TrendingProjectsActivity : BaseMviActivity<TrendingProjectsIntent, TrendingProjectsState>() {
 
@@ -40,30 +44,9 @@ class TrendingProjectsActivity : BaseMviActivity<TrendingProjectsIntent, Trendin
 
     override fun initView() {
         setSupportActionBar(toolbar)
-        toolbar.overflowIcon = ContextCompat.getDrawable(this, R.drawable.more_black)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         rvTrendingProjects.adapter = adapter
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.trending, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle item selection
-        return when (item.itemId) {
-            R.id.item_sort_name -> {
-                //TODO
-                true
-            }
-            R.id.item_sort_star -> {
-                //TODO
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
+        ivMenu.setOnClickListener(::showPopupMenu)
     }
 
     override fun bind() {
@@ -77,21 +60,47 @@ class TrendingProjectsActivity : BaseMviActivity<TrendingProjectsIntent, Trendin
 
     override fun render(state: TrendingProjectsState) {
         Timber.d("New State: $state")
-        if(state.isLoading) {
+        if (state.isLoading) {
             showLoading()
         } else {
             hideLoading()
         }
-        if(state.error != null) {
+        if (state.error != null) {
             Timber.e(state.error)
         }
-        if(state.data.isNotEmpty()) {
+        if (state.data.isNotEmpty()) {
             showData(state.data)
         }
     }
 
+    private fun showPopupMenu(view: View) {
+        val layoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = layoutInflater.inflate(R.layout.layout_menu, null)
+
+        val popupWindow = PopupWindow(
+            popupView,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        popupWindow.isOutsideTouchable = true
+        popupView.tvMenuName.setOnClickListener {
+            //TODO: Fire Sort Intent
+            popupWindow.dismiss()
+        }
+        popupView.tvMenuStars.setOnClickListener {
+            //TODO: Fire Sort Intent
+            popupWindow.dismiss()
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            popupWindow.elevation = 10f
+        }
+
+        popupWindow.showAsDropDown(view)
+    }
+
     private fun showLoading() {
-        if(swipeRefresh.isVisible()) {
+        if (swipeRefresh.isVisible()) {
             swipeRefresh.isRefreshing = true
         } else {
             shimmerLayout.visible()
@@ -100,7 +109,7 @@ class TrendingProjectsActivity : BaseMviActivity<TrendingProjectsIntent, Trendin
     }
 
     private fun hideLoading() {
-        if(swipeRefresh.isVisible()) {
+        if (swipeRefresh.isVisible()) {
             swipeRefresh.isRefreshing = false
         } else {
             shimmerLayout.stopShimmer()
